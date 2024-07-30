109136e8517370cf4f20846799c8956299b7c782
laneshields <lane.shields@gmail.com>
2024-07-30 18:39:55 -0400

    Update setup.sh
    Updated to be fully executable via curl

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
-python3 -m venv --without-pip venv
-source venv/bin/activate
-curl https://bootstrap.pypa.io/pip/3.6/get-pip.py | python
-deactivate
+#!/bin/bash -x
+echo "Pulling script from github"
+curl -o watch_pcap https://raw.githubusercontent.com/laneshields/ssr-pcap-watcher/main/watch_pcap.py &> /dev/null
+chmod +x watch_pcap &> /dev/null
+echo "Creating python virtual environment..."
+python3 -m venv --without-pip venv &> /dev/null
+source venv/bin/activate &> /dev/null
+echo "Installing pip and needed packages..."
+curl https://bootstrap.pypa.io/pip/3.6/get-pip.py | python &> /dev/null
+pip install pyinotify &> /dev/null
+deactivate &> /dev/null
+echo "Done. Use \"./watch_pcap -h\" for instructions"
